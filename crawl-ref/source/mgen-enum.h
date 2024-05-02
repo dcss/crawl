51b5d5de1928efcc06d1ed71805e76aca13d37bc
mumra <pete.hurst@gmail.com>
2024-05-02 22:23:56 +0100

    Balloon Yak: new Zot monster (regret-index)
    A chaos-themed yak variant found in the realm of Zot. On death bursts with a wind blast and a spread of chaotic clouds. During life, sucks the player towards it. Spawns in rather smaller packs than more mundane yaks as the clouds could get a bit ridiculous.
    
    Made by a street performer wizard.

--- crawl-ref/source/mgen-enum.h
+++ crawl-ref/source/mgen-enum.h
@@ -132,6 +132,7 @@ enum band_type
     BAND_WEEPING_SKULLS,
     BAND_THERMIC_DYNAMOS,
     BAND_ORB_SPIDERS,
+    BAND_BALLOON_YAKS,
     NUM_BANDS                   // always last
 };
 
--- crawl-ref/source/monster-type.h
+++ crawl-ref/source/monster-type.h
@@ -92,6 +92,9 @@ enum monster_type                      // env.mons[].type
 #endif
     MONS_YAK,
     MONS_DEATH_YAK,
+#if TAG_MAJOR_VERSION > 34
+    MONS_BALLOON_YAK,
+#endif
     MONS_CATOBLEPAS,
     MONS_ELEPHANT,
     MONS_DIRE_ELEPHANT,
@@ -1259,6 +1262,7 @@ enum monster_type                      // env.mons[].type
     MONS_MARROWCUDA,
     MONS_UNDYING_ARMOURY,
     MONS_XAKKRIXIS,
+    MONS_BALLOON_YAK,
 #endif
 
     NUM_MONSTERS,               // used for polymorph
